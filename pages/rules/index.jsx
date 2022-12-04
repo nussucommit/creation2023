995e29b2eb124126d95ea283ebd3f17e6014b1a3
Desmond To Cie Hong <desmond0429@Desmonds-MacBook-Pro-2.local>
2022-12-04 20:35:48 +0800

    Add rules

--- pages/rules/index.jsx
+++ pages/rules/index.jsx
@@ -1,5 +1,49 @@
+import { Fragment } from 'react';
+import rules from './rules.json';
+
 function RulePage() {
-  return <h1>RulePage</h1>;
+  return (
+    <>
+      <h1>Rules and Regulations</h1>
+      <ul>
+        {rules.map((rule) => {
+          const { details } = rule;
+
+          // Rule title
+          return (
+            <Fragment key={rule.title}>
+              <h3>{rule.title}</h3>
+              {details.map((detail) => {
+                if (typeof detail === 'object') {
+                  const detailTitle = detail.title;
+                  const { subdetails } = detail;
+
+                  // Rule detail, with subdetails
+                  return (
+                    <li key={detailTitle}>
+                      <p>{detailTitle}</p>
+                      {subdetails.map((subdetail) => (
+                        <ul key={subdetail}>
+                          <li>{subdetail}</li>
+                        </ul>
+                      ))}
+                    </li>
+                  );
+                }
+
+                // Rule detail, without subdetails
+                return (
+                  <li key={detail}>
+                    <p>{detail}</p>
+                  </li>
+                );
+              })}
+            </Fragment>
+          );
+        })}
+      </ul>
+    </>
+  );
 }
 
 export default RulePage;
