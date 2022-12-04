621e1097da97ed27101ae512a91365a03b7a95b4
Desmond To Cie Hong <desmond0429@Desmonds-MacBook-Pro-2.local>
2022-12-04 21:05:44 +0800

    Add contact

--- pages/contacts/index.jsx
+++ pages/contacts/index.jsx
@@ -1,5 +1,17 @@
 function ContactPage() {
-  return <h1>ContactPage</h1>;
+  return (
+    <>
+      <h1>Contact Us</h1>
+      <p>
+        Please submit your inquiry to
+        {' '}
+        <a href="mailto: creation@nussucommit.com">creation@nussucommit.com</a>
+        .
+        {' '}
+        Thank you :).
+      </p>
+    </>
+  );
 }
 
 export default ContactPage;
