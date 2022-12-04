0f15e059a15c927ea6ac7e80ca199f0319b195e2
Desmond To Cie Hong <desmond0429@Desmonds-MacBook-Pro-2.local>
2022-12-04 21:01:01 +0800

    Add faq

--- pages/faq/index.jsx
+++ pages/faq/index.jsx
@@ -1,5 +1,19 @@
+import faqs from './faq.json';
+
 function FAQPage() {
-  return <h1>FAQPage</h1>;
+  return (
+    <>
+      <h1>Frequently Asked Questions</h1>
+      <ol>
+        {faqs.map((faq) => (
+          <li key={faq.question}>
+            <h3>{faq.question}</h3>
+            {faq.answers.map((answer) => <p key={answer}>{answer}</p>)}
+          </li>
+        ))}
+      </ol>
+    </>
+  );
 }
 
 export default FAQPage;
