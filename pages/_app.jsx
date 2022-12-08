031f3bf086ff0ec88070926bd570469d13936dcf
Desmond To Cie Hong <desmond0429@Desmonds-MacBook-Pro-2.local>
2022-12-08 16:26:12 +0800

    Add root component

---
+++ pages/_app.jsx
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types';
+
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.element.isRequired,
+  pageProps: PropTypes.node.isRequired,
+};
+
+export default MyApp;
