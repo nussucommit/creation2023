675cf35fb4481b7135fc8803e90e4d6c9647931e
Desmond To Cie Hong <desmond0429@Desmonds-MacBook-Pro-2.local>
2022-12-11 00:29:50 +0800

    Change add announcement URL

--- pages/announcements/new/index.jsx
+++ pages/announcements/new/index.jsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import NewAnnouncementForm from '../../components/announcement/NewAnnouncementForm';
+import NewAnnouncementForm from '../../../components/announcement/NewAnnouncementForm';
 
 function AnnouncementManager() {
   const router = useRouter();
