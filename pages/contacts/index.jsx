a71943db25925b1caeda4ad6ed7c9b86a17f284b
Desmond To Cie Hong <desmond0429@Desmonds-MacBook-Pro-2.local>
2022-12-25 00:11:26 +0800

    Complete FAQ page

--- pages/contacts/index.jsx
+++ pages/contacts/index.jsx
@@ -1,10 +1,11 @@
 import PageContainer from '../../components/layout/PageContainer';
+import styles from '../../styles/ContactPage.module.scss';
 
 function ContactPage() {
   return (
     <PageContainer
       sectionContents={[
-        <>
+        <div className={styles['contact-item']}>
           <h1>Contact Us</h1>
           <p>
             Please submit your inquiry to
@@ -15,9 +16,17 @@ function ContactPage() {
             >
               creation@nussucommit.com
             </a>
-            . Thank you :)
+            .
+            {' '}
+            <br />
+            Thank you :)
           </p>
-        </>,
+          <img
+            id={styles['contact-decoration']}
+            src="/decorations/friends_orange.png"
+            alt="orange friends"
+          />
+        </div>,
       ]}
     />
   );
