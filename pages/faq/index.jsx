0d8d480e0b54056444c2407673c45f1e39dcd4fd
Desmond To Cie Hong <desmond0429@Desmonds-MacBook-Pro-2.local>
2022-12-24 23:57:02 +0800

    Complete FAQ page

--- pages/faq/index.jsx
+++ pages/faq/index.jsx
@@ -1,23 +1,34 @@
 import faqs from './faq.json';
 import PageContainer from '../../components/layout/PageContainer';
+import styles from '../../styles/FAQPage.module.scss';
 
 function FAQPage() {
   return (
     <PageContainer
       sectionContents={[
-        <>
-          <h1>Frequently Asked Questions</h1>
-          <ol>
-            {faqs.map((faq) => (
-              <li key={faq.question}>
-                <h3>{faq.question}</h3>
-                {faq.answers.map((answer) => (
-                  <p key={answer}>{answer}</p>
-                ))}
-              </li>
-            ))}
-          </ol>
-        </>,
+        <div>
+          <div className={styles['title-container']}>
+            <img
+              id={styles['faq-decoration']}
+              src="/decorations/flower_orange.png"
+              alt="orange flower"
+            />
+            <h1>Frequently Asked Questions</h1>
+          </div>
+          {faqs.map((faq) => (
+            <div key={faq.question} className={styles['faq-item']}>
+              <div className={styles['faq-question']}>
+                <img src="/decorations/question.png" alt="qustion" />
+                <div>
+                  <h2>{faq.question}</h2>
+                  {faq.answers.map((answer) => (
+                    <p key={answer}>{answer}</p>
+                  ))}
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>,
       ]}
     />
   );
