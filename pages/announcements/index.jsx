7115a0b88f11f7731cf0ec15a104533b4e84bff8
Desmond To Cie Hong <desmond0429@Desmonds-MacBook-Pro-2.local>
2022-12-24 22:35:36 +0800

    Fix announcement wrong order issue

--- pages/announcements/index.jsx
+++ pages/announcements/index.jsx
@@ -3,12 +3,10 @@ import PropTypes from 'prop-types';
 
 import PageContainer from '../../components/layout/PageContainer';
 import AnnouncementItem from '../../components/announcement/AnnouncementItem';
+import monthNames from '../../constants/monthNames';
 
 function AnnouncementPage({ isAuthorized, announcements }) {
   const hasAnnouncement = announcements.length !== 0;
-  if (hasAnnouncement) {
-    announcements.reverse();
-  }
 
   return (
     <PageContainer
@@ -54,7 +52,10 @@ export async function getStaticProps() {
 
   const announcementsCollection = db.collection('announcements');
 
-  const announcements = await announcementsCollection.find().toArray();
+  const announcements = await announcementsCollection
+    .find()
+    .sort({ datetime: -1 })
+    .toArray();
 
   client.close();
 
@@ -66,11 +67,33 @@ export async function getStaticProps() {
           '<br />',
         );
 
+        const datetime = new Date(announcement.datetime);
+        const day = datetime.getDate();
+        const month = monthNames[datetime.getMonth()];
+        let hour = datetime.getHours();
+        let meridiemIndicator = 'AM';
+
+        // Update meridiem indicator if necessary
+        if (hour >= 12) {
+          meridiemIndicator = 'PM';
+        }
+
+        // Convert to 12-hour clock format
+        if (hour > 12) {
+          hour -= 12;
+        }
+
+        if (hour === 0) {
+          hour = 12;
+        }
+
+        const datetimeString = `${day} ${month} ${hour}:${datetime.getMinutes()} ${meridiemIndicator}`;
+
         return {
           // eslint-disable-next-line no-underscore-dangle
           id: announcement._id.toString(),
           title: announcement.title,
-          datetime: announcement.datetime,
+          datetime: datetimeString,
           description,
           mediaURL: announcement.mediaURL,
           mediaType: announcement.mediaType,
