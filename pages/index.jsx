ef29a79eb2904c6d75e94bb43c179d66015d7a2f
Desmond To Cie Hong <desmond0429@Desmonds-MacBook-Pro-2.local>
2022-12-24 12:46:49 +0800

    Complete overview of challenges content

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,37 @@
 import PageContainer from '../components/layout/PageContainer';
+import HomePageData from './home-page-data.json';
+import styles from '../styles/HomePage.module.scss';
 
 function HomePage() {
   return (
     <PageContainer
       sectionContents={[
-        <h1 key="hero">CREATION 2023</h1>,
-        <h1 key="challenge-overview">Overview of Challenges</h1>,
-        <h1 key="competition-timeline">Competition Timeline</h1>,
-        <h1 key="prizes">Prizes</h1>,
-        <h1 key="partners">Partners</h1>,
-        <h1 key="sponsors">Sponsors</h1>,
+        <div key="hero">
+          <h1>CREATION 2023</h1>
+        </div>,
+        <div key="challenge-overview" className={styles['challenge-overview']}>
+          <h1>Overview of Challenges</h1>
+          {HomePageData['challenge-overview'].map((challenge) => (
+            <div key={challenge.title}>
+              <h3>{challenge.title}</h3>
+              <hr />
+              <p>{challenge.description}</p>
+              <a href={challenge.link}>Learn more...</a>
+            </div>
+          ))}
+        </div>,
+        <div key="competition-timeline">
+          <h1>Competition Timeline</h1>
+        </div>,
+        <div key="prizes">
+          <h1>Prizes</h1>
+        </div>,
+        <div key="partners">
+          <h1>Partners</h1>
+        </div>,
+        <div key="sponsors">
+          <h1>Sponsors</h1>
+        </div>,
       ]}
     />
   );
