b2b6d57c922612854ba92c2092c5b945add619ff
Desmond To Cie Hong <desmond0429@Desmonds-MacBook-Pro-2.local>
2022-12-24 18:30:56 +0800

    Remove snap scrolling effect

--- components/layout/SectionContainer.jsx
+++ components/layout/SectionContainer.jsx
@@ -1,33 +1,9 @@
-import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import NavBar from '../ui/NavBar';
-import BREAKPOINTS from '../../constants/breakPoints';
 import styles from './SectionContainer.module.scss';
 
-function SectionContainer({ sectionCount, sectionIndex, content }) {
-  const [windowWidth, setWindowWidth] = useState(0);
-
-  useEffect(() => {
-    function handleResize() {
-      setWindowWidth(window.innerWidth);
-    }
-
-    window.addEventListener('resize', handleResize);
-
-    handleResize();
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, [setWindowWidth]);
-
-  // If it is a homepage (more than 1 section) and large screen,
-  // enable scroll snap align start and hide section container scroll bar
-  const scrollClass = sectionCount > 1 && windowWidth > BREAKPOINTS.lg
-    ? `${styles['snap-start']} ${styles['scrollbar-hide']}`
-    : '';
-
+function SectionContainer({ sectionIndex, content }) {
   // Alternate the background color and text color of the section
   const sectionClass = sectionIndex % 2 === 0
     ? styles['primary-section']
@@ -35,7 +11,7 @@ function SectionContainer({ sectionCount, sectionIndex, content }) {
 
   return (
     <section
-      className={`${styles['section-container']} ${scrollClass} ${sectionClass}`}
+      className={`${styles['section-container']} ${sectionClass}`}
     >
       {sectionIndex === 0 && <NavBar />}
       {content}
@@ -44,7 +20,6 @@ function SectionContainer({ sectionCount, sectionIndex, content }) {
 }
 
 SectionContainer.propTypes = {
-  sectionCount: PropTypes.number.isRequired,
   sectionIndex: PropTypes.number.isRequired,
   content: PropTypes.oneOfType([PropTypes.elementType, PropTypes.node])
     .isRequired,
