67bf99d95c5d288945f6ed146ceb835762a349db
Desmond To Cie Hong <desmond0429@Desmonds-MacBook-Pro-2.local>
2022-12-24 23:32:40 +0800

    Complete rule page

--- pages/rules/index.jsx
+++ pages/rules/index.jsx
@@ -1,22 +1,28 @@
-import { Fragment } from 'react';
-
 import rules from './rules.json';
 import PageContainer from '../../components/layout/PageContainer';
+import styles from '../../styles/RulePage.module.scss';
 
 function RulePage() {
   return (
     <PageContainer
       sectionContents={[
-        <>
-          <h1>Rules and Regulations</h1>
-          <ul>
-            {rules.map((rule) => {
-              const { details } = rule;
-
-              // Rule title
-              return (
-                <Fragment key={rule.title}>
-                  <h3>{rule.title}</h3>
+        <div>
+          <div className={styles['title-container']}>
+            <h1>Rules and Regulations</h1>
+            <img
+              id={styles['rule-decoration']}
+              src="/decorations/butterfly_orange.png"
+              alt="orange butterfly"
+            />
+          </div>
+          {rules.map((rule) => {
+            const { details } = rule;
+            // Rule title
+            return (
+              <div key={rule.title} className={styles['rule-item']}>
+                <h2>{rule.title}</h2>
+                <hr />
+                <ul>
                   {details.map((detail) => {
                     if (typeof detail === 'object') {
                       const detailTitle = detail.title;
@@ -42,11 +48,11 @@ function RulePage() {
                       </li>
                     );
                   })}
-                </Fragment>
-              );
-            })}
-          </ul>
-        </>,
+                </ul>
+              </div>
+            );
+          })}
+        </div>,
       ]}
     />
   );
