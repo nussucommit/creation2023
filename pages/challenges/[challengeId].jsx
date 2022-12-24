41365c6f5b08281ebabf1071b3080141f7a53f17
Desmond To Cie Hong <desmond0429@Desmonds-MacBook-Pro-2.local>
2022-12-24 23:02:43 +0800

    Complete challenge page layout

--- pages/challenges/[challengeId].jsx
+++ pages/challenges/[challengeId].jsx
@@ -1,14 +1,31 @@
 import { useRouter } from 'next/router';
 import PageContainer from '../../components/layout/PageContainer';
+import CHALLENGES from './challenges.json';
+import styles from '../../styles/ChallengePage.module.scss';
 
 function ChallengeDetailPage() {
   const router = useRouter();
 
   const { challengeId } = router.query;
+  const challengeIndex = +challengeId - 1;
+  const challengeData = CHALLENGES[challengeIndex];
 
-  return (
-    <PageContainer sectionContents={[<h1>{`Challenge #${challengeId}`}</h1>]} />
-  );
+  if (challengeData) {
+    return (
+      <PageContainer
+        sectionContents={[
+          <>
+            <h1>Challenges</h1>
+            <div className={styles['challenge-item']}>
+              <h2>{challengeData.title}</h2>
+              <hr />
+              <p>{challengeData.description}</p>
+            </div>
+          </>,
+        ]}
+      />
+    );
+  }
 }
 
 export default ChallengeDetailPage;
