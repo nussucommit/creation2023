2f50c7e185295d70aff582c6170d3c35453ca57a
Desmond To Cie Hong <70432640+DesmondTo@users.noreply.github.com>
2022-12-24 23:10:47 +0800

    Challenge page (#5)
    * Complete challenge page layout
    
    * Add decoration for challenge page
    
    Co-authored-by: Desmond To Cie Hong <desmond0429@Desmonds-MacBook-Pro-2.local>

--- pages/challenges/[challengeId].jsx
+++ pages/challenges/[challengeId].jsx
@@ -1,14 +1,38 @@
 import { useRouter } from 'next/router';
 import PageContainer from '../../components/layout/PageContainer';
+import CHALLENGES from './challenges.json';
+import styles from '../../styles/ChallengePage.module.scss';
 
 function ChallengeDetailPage() {
   const router = useRouter();
 
   const { challengeId } = router.query;
+  const challengeIndex = +challengeId - 1;
+  const challengeData = CHALLENGES[challengeIndex];
 
-  return (
-    <PageContainer sectionContents={[<h1>{`Challenge #${challengeId}`}</h1>]} />
-  );
+  if (challengeData) {
+    return (
+      <PageContainer
+        sectionContents={[
+          <>
+            <div className={styles['title-container']}>
+              <h1>Challenges</h1>
+              <img
+                id={styles['challenge-decoration']}
+                src="/decorations/paperplane_orange.png"
+                alt="orange paper plane"
+              />
+            </div>
+            <div className={styles['challenge-item']}>
+              <h2>{challengeData.title}</h2>
+              <hr />
+              <p>{challengeData.description}</p>
+            </div>
+          </>,
+        ]}
+      />
+    );
+  }
 }
 
 export default ChallengeDetailPage;
