2f6d08e2367aec4c748d4c46bc212c34a27d8f14
Desmond To Cie Hong <desmond0429@Desmonds-MacBook-Pro-2.local>
2022-12-25 17:18:33 +0800

    Add font loader helper functions

---
+++ helper/font-loader.js
@@ -0,0 +1,6 @@
+import { Inter, Kavoon } from '@next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+export const kavoon = Kavoon({ weight: '400', subsets: ['latin'] });
+
+export default inter;
