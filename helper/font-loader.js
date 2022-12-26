6ba372d0050b61d3a3b8f78b203509b277a55a63
Desmond To Cie Hong <desmond0429@Desmonds-MacBook-Pro-2.local>
2022-12-26 17:44:04 +0800

    Guarantee display of title font

--- helper/font-loader.js
+++ helper/font-loader.js
@@ -1,6 +1,10 @@
 import { Inter, Kavoon } from '@next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
-export const kavoon = Kavoon({ weight: '400', subsets: ['latin'] });
+export const kavoon = Kavoon({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+});
 
 export default inter;
