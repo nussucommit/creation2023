e2fc7bed4e2f868a34061d5da4852da335b79a8e
Desmond To Cie Hong <desmond0429@Desmonds-MacBook-Pro-2.local>
2022-12-29 08:50:19 +0800

    Force use HTTPS on production

--- middleware.js
+++ middleware.js
@@ -2,6 +2,18 @@ import { NextResponse } from 'next/server';
 import { CHALLENGE_LINKS } from './constants/navbarLinks';
 
 function middleware(request) {
+  const currentEnv = process.env.ENV;
+
+  if (
+    currentEnv === 'PROD'
+    && request.headers.get('x-forwarded-proto') !== 'https'
+  ) {
+    return NextResponse.redirect(
+      `https://${request.headers.get('host')}${request.nextUrl.pathname}`,
+      301,
+    );
+  }
+
   const url = request.nextUrl.clone();
   if (url.pathname === '/challenges') {
     const firstChallengePath = CHALLENGE_LINKS[0].path;
@@ -9,7 +21,7 @@ function middleware(request) {
     return NextResponse.redirect(url);
   }
 
-  return NextResponse.rewrite(url);
+  return NextResponse.next();
 }
 
 export default middleware;
